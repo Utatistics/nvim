a5cd2f1db7cfd09d63a84829dc46d6c775b69b2c
Utashi Fujimoto <frusciante2580@gmail.com>
2026-05-17 12:21:45 +0000

    add: minor modifications

--- lua/snippets/binarySearch.cpp
+++ lua/snippets/binarySearch.cpp
@@ -1,4 +1,4 @@
-int binary_search(int key, const::std::vector<int> &A) {
+int binary_search(int key, const std::vector<int> &A) {
     int left = -1; // i = 0 does not always satisfy condition
     int right = (int)A.size();
     while (right - left > 1) {

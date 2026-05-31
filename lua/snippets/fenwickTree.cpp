402ddd35d8a44c0178d47d7547cb57f6095702bd
Utashi Fujimoto <frusciante2580@gmail.com>
2026-05-31 23:38:49 +0000

    add: annotation

--- lua/snippets/fenwickTree.cpp
+++ lua/snippets/fenwickTree.cpp
@@ -1,25 +1,25 @@
 using ll = long long;
 
 struct fenwickTree {
-    int n;
-    std::vector<ll> t; // internal array
+    int N;
+    std::vector<ll> A; // internal array
 
-    fenwickTree(int n) : n(n), t(n + 1, 0) {} // 0th index unused
+    fenwickTree(int n) : N(n), A(n + 1, 0) {} // 0th index unused
 
     void update(int i, ll x) { // add x to index i
-        while (i <= n) {
-            t[i] += x;
-            i += i & -i;
+        while (i <= N) {
+            A[i] += x; // iterative update
+            i += i & -i; // add LSBs to get next index
         }
     }
 
     ll query(int i) { // sum from 1 to i
-        ll res = 0;
+        ll s = 0; 
         while (i > 0) {
-            res += t[i];
-            i -= i & -i;
+            s += A[i]; // iterative addition to cumsum
+            i -= i & -i; // substruct LSBs to get next index
         }
-        return res;
+        return s;
     }
 
     ll range(int l, int r) { // sum from l to r

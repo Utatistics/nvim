206d6f188b1056a9f369fb2735afab7ad1cf9235
Utashi Fujimoto <frusciante2580@gmail.com>
2026-08-16 13:17:51 +0000

    add: modpow

--- lua/snippets/mod.cpp
+++ lua/snippets/mod.cpp
@@ -1,2 +1,13 @@
 // mathematical modulo (safely assume [0, b-1])
 int mod(int a, int b) {return (a % b + b) % b;}
+
+ll modpow(ll a, ll n) { // a ^ N (mod MOD) 
+    ll x = 1;
+
+    while (n > 0) { // e.g. a^13 = a^0 * a^4 * a^8
+        if (n & 1) x = x * a % MOD; // if lowest binary active -> x *= a
+        a = a * a % MOD;
+        n >>= 1; // shift to next binary
+    }
+    return x;
+}
